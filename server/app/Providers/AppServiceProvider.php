9ea9d3158d217b441e9e6f10c0d8f0daf3a3855b
ronny torres <ronnytorresmtz@gmail.com>
2017-11-09 09:02:11 -0600

    add this file

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use MyCode\Repositories\Company\CompanyRepository;
+use MyCode\Models\Company;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+       
+        $this->app->bind('MyCode\Repositories\Company\CompanyRepositoryInterface', function($app) 
+        {
+  
+          return new CompanyRepository(new Company);
+  
+        });
+
+        $this->app->bind('MyCode\Services\Mail\MailServiceInterface','MyCode\Services\Mail\MailService');
+        
+        $this->app->bind('MyCode\Services\Document\DocumentServiceInterface','MyCode\Services\Document\DocumentService');
+
     }
 }
