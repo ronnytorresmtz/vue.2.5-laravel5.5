741b15d4f3b9377081873052f3897bcd963f5ec2
ronny torres <ronnytorresmtz@gmail.com>
2017-11-09 08:21:08 -0600

    create directory and file store.js with some configuracion for jquery, no-cosole, no-alert and "_"

---
+++ client/src/components/store/Store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+// import Axios from 'axios';
+
+Vue.use(Vuex);
+// Vue.prototype.$http = Axios;
+
+const store = new Vuex.Store({
+  modules: {
+    
+  },
+  state: {
+    
+  },
+  mutations: {
+    
+  },
+  actions: {
+    
+  },
+  getters: {
+    
+  },
+});
+
+export default store;
